42d9155e35378b232344f4ac5be1e8f5072a0edd
Niskarsh Kumar <niskarshk@gmail.com>
2018-07-07 19:29:20 +0530

    Added express, index.js, App.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import App from './server/App'
+
+ReactDOM.render (
+    <App />,
+    document.getElementById('root'))
---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+
+let app = express()
+
+app.use (express.static( `${__dirname}/../public`))
+
+const port = process.env.PORT || 3000
+
+app.get ('/', (req, res) => {
+    res.send ('index.html')
+})
+
+
+app.listen (port, console.log (`Started listening on port : ${port}`))
